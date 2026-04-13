868b6402960db87122a6358a1800cf7f025faeb7
remco marien <remco.marien@ugent.be>
2026-04-13 19:30:05 +0200

    test commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from settings import *
+def printtest():
+    print("test")
+    print(torch.__version__)
+
+
+if __name__ == "__main__":
+    printtest()
---
+++ src/settings.py
@@ -0,0 +1,14 @@
+#import visualisation tooling
+import matplotlib.pyplot as plt
+
+#import deep learning libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+torch.set_printoptions(precision=2)
+torch.manual_seed(123)
+
+#import data processing libraries
+import numpy as np
+import pandas as pd
